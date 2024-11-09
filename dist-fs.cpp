a4f8b1c310274267b05f7499026a052d47a9686a
Akiel Aries <akiel@planet.com>
2024-11-08 22:07:12 -0800

    cpp vs c for reading is a bit different

--- dist-fs.cpp
+++ dist-fs.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <string>
+#include <vector>
+
+#include <cstdlib>
+#include <stdio.h>  // I prefer printf
 
 #include "dist-fs/file_format.hpp"
 
-
+std::string hex_to_ascii(unsigned int hexValue) {
+  std::ostringstream oss;
+  for (int i = sizeof(hexValue) - 1; i >= 0; --i) {
+    char ch = (hexValue >> (i * 8)) & 0xFF;
+    if (ch) {
+      oss << ch;
+    }
+  }
+  return oss.str();
+}
 
 int main(int argc, char *argv[]) {
-  std::cout << "dist-fs app\n";
+  printf("dist-fs app\n\n");
 
+  if (argc < 2) {
+    printf("Pass in a file to read...\n");
+    return -1;
+  }
+
+  // read in the file (argv[1]) as a binary file
+  std::ifstream audio_file(argv[1]);
+  if (!audio_file) {
+    printf("No such file %s\n", argv[1]);
+  }
+
+  std::vector<char> audio_file_data((std::istreambuf_iterator<char>(audio_file)),
+                              std::istreambuf_iterator<char>());  
+  if (audio_file_data.size() < DIST_FS_RIFF_SZ) {
+    printf("File is too small to contain a valid RIFF header... perhaps this should be different\n");
+  }
+
+  std::cout << "Audio file data: " << std::endl;
+  for (uint8_t i = 0; i < DIST_FS_RIFF_SZ ; i++) {
+    std::cout << audio_file_data[i];
+  }
+
+
+  std::string riff = hex_to_ascii(DIST_FS_RIFF);
+
+  printf("RIFF chunk desc: \n");
+  printf("\tChunk ID: 0x%0X (%s) \n", DIST_FS_RIFF, riff.c_str() );
+
+  
+  audio_file.close();
 
   return 0;
 }
--- dist-fs/file_format.hpp
+++ dist-fs/file_format.hpp
@@ -1,1 +1,8 @@
 #pragma once
+
+/** some definitions related to the wav file format */
+#define DIST_FS_RIFF 0x52494646
+
+#define DIST_FS_RIFF_SZ 4
+
+
