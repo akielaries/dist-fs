283b2e8ecda5ade0aa5fd3b7bd6e855ac707d5ab
akielaries <akiel@akiel.org>
2024-12-15 04:13:54 +0000

    forgot headers

--- dist-fs/comms/network.c
+++ dist-fs/comms/network.c
@@ -1,4 +1,12 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include "comms.h"
 
